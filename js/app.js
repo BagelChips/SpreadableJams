f24facca50ec612f77a0c8e354da228e456dc656
Mark Riddell <mark.andrew.riddell@gmail.com>
2021-06-02 15:55:02 -0400

    Adds Working Modal Logic and Polishes Mobile CSS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-const submitBtn = document.querySelector(".submit");
+"use strict";
+
 const communityBtn = document.querySelector(".communityBtn");
 const featuredBtn = document.querySelector(".featuredBtn");
 const featuredContent = document.querySelector(".featuredContent");
 const communityContent = document.querySelector(".communityContent");
+
+// --Modal Functionality---
+const modalBtn = document.querySelector(".modal-button");
+const modalBg = document.querySelector(".modal-bg");
+const modalClose = document.querySelector(".modal-close");
+
+modalBtn.addEventListener("click", () => {
+  modalBg.classList.add("visible");
+});
+modalClose.addEventListener("click", () => {
+  modalBg.classList.remove("visible");
+});
 
 communityBtn.addEventListener("click", () => {
   toggleHide(featuredBtn);
@@ -23,5 +36,14 @@ function toggleHide(element) {
     element.classList.remove("hide");
   } else {
     element.classList.add("hide");
+  }
+}
+
+// Search object?
+
+function search(track, artist) {
+  let query = { track, artist };
+  for (let key in query) {
+    console.log(key + "=" + query[key]);
   }
 }
