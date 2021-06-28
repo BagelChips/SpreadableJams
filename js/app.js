58e8ab107c62e392f93f9523b59c1a18ad9d9b42
Mark Riddell <mark.andrew.riddell@gmail.com>
2021-06-28 14:04:58 -0400

    very minor update to UI

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ const communityBtn = document.querySelector(".communityBtn");
 const featuredBtn = document.querySelector(".featuredBtn");
 const featuredContent = document.querySelector(".featuredContent");
 const communityContent = document.querySelector(".communityContent");
+const contentAndButtons = [
+  communityBtn,
+  featuredBtn,
+  featuredContent,
+  communityContent,
+];
+
+//
 
 // --Modal Functionality---
+
 const modalBtn = document.querySelector(".modal-button");
 const modalBg = document.querySelector(".modal-bg");
 const modalClose = document.querySelector(".modal-close");
@@ -13,37 +22,25 @@ const modalClose = document.querySelector(".modal-close");
 modalBtn.addEventListener("click", () => {
   modalBg.classList.add("visible");
 });
+
 modalClose.addEventListener("click", () => {
   modalBg.classList.remove("visible");
 });
 
 communityBtn.addEventListener("click", () => {
-  toggleHide(featuredBtn);
-  toggleHide(featuredContent);
-  toggleHide(communityBtn);
-  toggleHide(communityContent);
+  toggleClass(contentAndButtons, "hide");
 });
 
 featuredBtn.addEventListener("click", () => {
-  toggleHide(featuredBtn);
-  toggleHide(featuredContent);
-  toggleHide(communityBtn);
-  toggleHide(communityContent);
+  toggleClass(contentAndButtons, "hide");
 });
 
-function toggleHide(element) {
-  if (element.classList.contains("hide")) {
-    element.classList.remove("hide");
-  } else {
-    element.classList.add("hide");
+let toggleClass = function (elements, className) {
+  for (let each of elements) {
+    if (each.classList.contains(className)) {
+      each.classList.remove(className);
+    } else {
+      each.classList.add(className);
+    }
   }
-}
-
-// Search object?
-
-function search(track, artist) {
-  let query = { track, artist };
-  for (let key in query) {
-    console.log(key + "=" + query[key]);
-  }
-}
+};
